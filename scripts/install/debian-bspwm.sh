c369f59a60522c009e17dbbf8f02d349e40a57b2
R0M <36591161+romish17@users.noreply.github.com>
2024-08-20 00:50:49 +0200

    update debian-bspwm

--- scripts/install/debian-bspwm.sh
+++ scripts/install/debian-bspwm.sh
@@ -63,5 +63,4 @@ wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/do
 && cd ~/.local/share/fonts \
 && unzip JetBrainsMono.zip \
 && rm JetBrainsMono.zip \
-&& fc-cache -fv 2> /dev/null
-
+&& fc-cache -fv
