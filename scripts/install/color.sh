662397d6254d4abf9b26cef76aa66829448e1c38
R0M <36591161+romish17@users.noreply.github.com>
2024-08-20 00:33:09 +0200

    Update rofi & alacritty

---
+++ scripts/install/color.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Color variables
+LIGHTGREEN='\033[1;32m'
+YELLOW='\033[0;93m'
+RED='\033[0;31m'
+LIGHT='\033[2;32m'
+RESET='\033[0m'
--- scripts/install/debian-bspwm.sh
+++ scripts/install/debian-bspwm.sh
@@ -1,31 +1,57 @@
 #!/usr/bin/env bash
 
+./color.sh
 # Add contrib & non-free repos
-sed -i 's/^deb \(\S*\) \(.*\) main/deb \1 \2 main contrib non-free/' /etc/apt/sources.list
+echo -e "${LIGHTGREEN}[Installing]${LIGHT} Ajout des dépôts contrib et non-free...${RESET}"
+sed -i 's/^deb \(\S*\) \(.*\) main/deb \1 \2 main contrib non-free/' /etc/apt/sources.list 2> /dev/null
 
-sudo apt update && apt upgrade -yy
-sudo apt install xorg bspwm sxhkd polybar compton rofi dunst nitrogen i3lock redshift cmus ranger kitty alacritty thunar unzip psmisc pulseaudio -yy
+sudo apt update && apt upgrade -yy 2> /dev/null
 
-# bspwm conf -> ok
-# sxhkdrc conf -> ok
-# polybar conf -> ok
-# compton conf -> ok
-# nanorc conf
-# btop conf
-# htop conf
-# theme
-# fonts
-# icons
+debian_apps=(
+    xorg
+    bspwm
+    sxhkd
+    polybar
+    compton
+    rofi
+    dunst
+    nitrogen
+    i3lock
+    redshift
+    cmus
+    ranger
+    kitty
+    alacritty
+    thunar
+    unzip
+    psmisc
+    pulseaudio
+)
 
-mkdir -p ~/.config/{bspwm,sxhkd,polybar,compton}
-cp ../linux/dotfiles/bspwm/bspwmrc ~/.config/bspwm/
-cp ../linux/dotfiles/sxhkd/sxhkdrc ~/.config/sxhkd/
-cp ../linux/dotfiles/polybar/config.ini ~/.config/polybar/
-cp ../linux/dotfiles/compton/compton.conf ~/.config/compton/compton.conf
-cp ../linux/dotfiles/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+for app in ${debian_apps[@]}; do
+    if hash "${app}" 2> /dev/null; then
+      echo -e "${YELLOW}[Skipping]${LIGHT} ${app} is already installed${RESET}"
+    elif hash flatpak 2> /dev/null && [[ ! -z $(echo $(flatpak list --columns=ref | grep $app)) ]]; then
+      echo -e "${YELLOW}[Skipping]${LIGHT} ${app} is already installed via Flatpak${RESET}"
+    else
+      echo -e "${LIGHTGREEN}[Installing]${LIGHT} Downloading ${app}...${RESET}"
+      sudo apt install ${app} --assume-yes 2> /dev/null
+    fi
+
+
+echo -e "${LIGHTGREEN}[Installing]${LIGHT} Copy theme files...${RESET}"
+
+#mkdir -p ~/.config/{bspwm,sxhkd,polybar,compton,gtk-3.0}
+#cp ../linux/dotfiles/bspwm/bspwmrc ~/.config/bspwm/
+#cp ../linux/dotfiles/sxhkd/sxhkdrc ~/.config/sxhkd/
+#cp ../linux/dotfiles/polybar/config.ini ~/.config/polybar/
+#cp ../linux/dotfiles/compton/compton.conf ~/.config/compton/compton.conf
+#cp ../linux/dotfiles/gtk-3.0/settings.ini ~/.config/gtk-3.0/settings.ini
+#cp ../linux/dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 cp -r ../../custom/wallpapers/ ~/.config/wallpapers/
+cp -r ../linux/dotfiles/ ~/.config/
 
-sudo mkdir -p /usr/share/icons/Nordzy/ && sudo tar -xzf ../../custom/icons/Nordzy.tar.gz && sudo cp -r Nordzy/ /usr/share/icons/Nordzy/
+sudo tar -xzf ../../custom/icons/Nordzy.tar.gz && sudo cp -r Nordzy/ /usr/share/icons/
 unzip ../../custom/themes/Nordic-master.zip
 sudo cp -r Nordic-master/ /usr/share/themes/Nordic/
 sudo cp -r ../../custom/fonts/*.ttf ~/.local/share/fonts/
@@ -37,5 +63,5 @@ wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/do
 && cd ~/.local/share/fonts \
 && unzip JetBrainsMono.zip \
 && rm JetBrainsMono.zip \
-&& fc-cache -fv
+&& fc-cache -fv 2> /dev/null
 
